0418b4c78e3dc47f57eac158e56e43eb04567cf7
YeonwooSung <neos960518@gmail.com>
2022-10-09 21:41:35 +0900

    Implement GCN

---
+++ src/gcn/gcn.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+
+
+class GraphConvolutionLayer(nn.Module):
+    def __init__(
+        self,
+        input_dim, 
+        output_dim, 
+        support, 
+        act_func = None, 
+        featureless = False, 
+        dropout_rate = 0., 
+        bias=False
+    ):
+        super(GraphConvolutionLayer, self).__init__()
+        self.support = support
+        self.featureless = featureless
+        for i in range(len(self.support)):
+            setattr(self, 'W{}'.format(i), nn.Parameter(torch.randn(input_dim, output_dim)))
+
+        if bias:
+            self.b = nn.Parameter(torch.zeros(1, output_dim))
+
+        self.act_func = act_func
+        self.dropout = nn.Dropout(dropout_rate)
+
+        
+    def forward(self, x):
+        x = self.dropout(x)
+        
+        for i in range(len(self.support)):
+            if self.featureless:
+                pre_sup = getattr(self, 'W{}'.format(i))
+            else:
+                pre_sup = x.mm(getattr(self, 'W{}'.format(i)))
+            
+            if i == 0:
+                out = self.support[i].mm(pre_sup)
+            else:
+                out += self.support[i].mm(pre_sup)
+
+        if self.act_func is not None:
+            out = self.act_func(out)
+
+        self.embedding = out
+        return out
+
+
+class GCN(nn.Module):
+    def __init__(self, 
+        input_dim, 
+        support,
+        dropout_rate=0., 
+        num_classes=10,
+        hidden_dim=200,
+    ):
+        super(GCN, self).__init__()
+        
+        # GraphConvolution
+        self.layer_in = GraphConvolutionLayer(input_dim, hidden_dim, support, act_func=nn.ReLU(), featureless=True, dropout_rate=dropout_rate)
+        self.layer_out = GraphConvolutionLayer(hidden_dim, num_classes, support, dropout_rate=dropout_rate)
+        
+    
+    def forward(self, x):
+        out = self.layer_in(x)
+        out = self.layer_out(out)
+        return out
