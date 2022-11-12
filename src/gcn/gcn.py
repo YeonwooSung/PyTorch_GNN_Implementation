f314fe342111b0e4c52dfebafc505f2bf95e0697
YeonwooSung <neos960518@gmail.com>
2022-11-12 23:27:15 +0900

    Reimplement GCN as a straightforward version

--- src/gcn/gcn.py
+++ src/gcn/gcn.py
@@ -1,76 +1,105 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
+from torch.nn import Parameter
+import math
 
 
-class GraphConvolutionLayer(nn.Module):
+class GCN_Layer(nn.Module):
     def __init__(
-        self,
-        input_dim, 
-        output_dim, 
-        support, 
-        act_func = None, 
-        featureless = False, 
-        dropout_rate = 0., 
-        bias=False
-    ):
-        super(GraphConvolutionLayer, self).__init__()
-        self.support = support
-        self.featureless = featureless
-        for i in range(len(self.support)):
-            setattr(self, 'W{}'.format(i), nn.Parameter(torch.randn(input_dim, output_dim)))
+        self, 
+        adjacent_matrix, 
+        input_size, 
+        hidden_size, 
+        dropout_prob=0.5, 
+        node_dim=0, 
+        bias=True
+    ) -> None:
+        super().__init__()
+        self.adjacent_matrix = adjacent_matrix
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.node_dim = node_dim
 
+        self.weight = Parameter(torch.FloatTensor(input_size, hidden_size))
         if bias:
-            self.b = nn.Parameter(torch.zeros(1, output_dim))
+            self.bias = Parameter(torch.FloatTensor(hidden_size))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
 
-        self.act_func = act_func
-        self.dropout = nn.Dropout(dropout_rate)
+        self.activation = nn.ReLU()
+        self.dropout1 = nn.Dropout(p=dropout_prob)
+        self.dropout2 = nn.Dropout(p=dropout_prob)
 
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, data):
+        #
+        # Step 1: Regularization
+        #
+        in_nodes_features, edge_index = data  # unpack data
+        num_of_nodes = in_nodes_features.shape[self.node_dim]
+        assert edge_index.shape[0] == 2, f'Expected edge index with shape=(2,E) got {edge_index.shape}'
+
+        # shape = (N, FIN) where N is the number of nodes in the graph, and FIN is the number of input features per node.
+        # We apply the dropout to all of the input node features 
+        # Note that for Cora features are already super sparse, so it is questionable how much this actually helps
+        in_nodes_features = self.dropout1(in_nodes_features)
+
+        # perform graph convolution
+        support = torch.mm(in_nodes_features, self.weight)
+        output = torch.spmm(self.adjacent_matrix, support)
+
+        # add bias if needed
+        if self.bias is not None:
+            output += self.bias
         
-    def forward(self, x):
-        x = self.dropout(x)
-        
-        for i in range(len(self.support)):
-            if self.featureless:
-                pre_sup = getattr(self, 'W{}'.format(i))
-            else:
-                pre_sup = x.mm(getattr(self, 'W{}'.format(i)))
-            
-            if i == 0:
-                out = self.support[i].mm(pre_sup)
-            else:
-                out += self.support[i].mm(pre_sup)
+        # pass output through activation function
+        output = self.activation(output)
+        output = self.dropout2(output)
 
-        if self.act_func is not None:
-            out = self.act_func(out)
+        return (output, edge_index)
 
-        self.embedding = out
-        return out
 
 
 class GCN(nn.Module):
-    def __init__(self, 
-        input_dim, 
-        support,
-        num_classes,
-        dropout_rate=0., 
-        hidden_dim=256,
-        num_hidden_layers=2,
-    ):
-        super(GCN, self).__init__()
-        
-        # GraphConvolution
-        self.layer_in = GraphConvolutionLayer(input_dim, hidden_dim, support, act_func=nn.ReLU(), featureless=True, dropout_rate=dropout_rate)
-        self.layer_out = GraphConvolutionLayer(hidden_dim, num_classes, support, dropout_rate=dropout_rate)
+    def __init__(
+        self,
+        adjacent_matrix,
+        num_of_layers,
+        num_features_per_layer,
+        dropout_prob=0.5, 
+        node_dim=0, 
+        bias=True
+    ) -> None:
+        super().__init__()
+        assert num_of_layers == len(num_features_per_layer) - 1, f'Enter valid arch params.'
 
-        self.num_hidden_layers = num_hidden_layers
-        self.hidden_layers = nn.ModuleList()
-        for i in range(num_hidden_layers):
-            self.hidden_layers.append(GraphConvolutionLayer(hidden_dim, hidden_dim, support, act_func=nn.ReLU(), dropout_rate=dropout_rate))
+        self.adjacent_matrix = adjacent_matrix
+        self.num_of_layers = num_of_layers
         
-    
+        gcn_layers = []
+        for i in range(num_of_layers):
+            layer = GCN_Layer(
+                adjacent_matrix,
+                num_features_per_layer[i],
+                num_features_per_layer[i+1],
+                dropout_prob=dropout_prob,
+                node_dim=node_dim,
+                bias=bias
+            )
+            gcn_layers.append(layer)
+        self.gcn_layers = nn.ModuleList(gcn_layers)
+
     def forward(self, x):
-        out = self.layer_in(x)
-        for i in range(self.num_hidden_layers):
-            out = self.hidden_layers[i](out)
-        out = self.layer_out(out)
-        return out
+        # The data is just a tuple of (node_features, edge_index)
+        for i in range(self.num_of_layers):
+            x = self.gcn_layers[i](x)
+        output_features, edge_index = x
+        output_features = F.log_softmax(output_features, dim=1)
+        return (output_features, edge_index)
